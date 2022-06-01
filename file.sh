ff1b8a34274cb7221d8cb888ba92c002eedb9848
daneng1 <danengel@dans-mbp-2.lan>
2022-06-01 15:47:37 -0700

    initial commit

---
+++ file.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+tput clear
+tput bold
+tput cup 0 15
+echo Please tell me hopw your day is going
+tput cup 1 10
+echo 1 for terrible
+tput cup 2 10
+echo 2 for not great
+tput cup 3 10
+echo 3 for so-so
+tput cup 4 10
+echo 4 for fairly good but nothing special
+tput cup 5 10
+echo 5 for amaziong, excellent
+
+tput bold
+tput cup 7 15
+read -rsn1 -p "Enter your choice, 1-5" choice
+
+tput cup 10 15
+
+returnMsg()
+{
+ if [ $choice = 1 ]; then
+  tput setaf 1
+  figlet -c -f poison Dumpster Fire
+elif [ $choice = 2 ]; then
+  tput setaf 5
+  figlet -c -f doh What the Hell??
+elif [ $choice = 3 ]; then
+  tput setaf 3
+  figlet -c -f dotmatrix Feels a bit chilly
+elif [ $choice = 4 ]; then
+  tput setaf 4
+  figlet -c -f epic Sunshine and Roses
+else
+  tput setaf 2
+  figlet -c -f lean Walking on Clouds
+fi
+}
+
+returnMsg
